fdcaf17dde17b062fc2bc74e936a03d72db56bd3
Harsh Gupta <harshgupta88156@gmail.com>
2026-01-22 12:08:19 +0530

    Added static files and templates

--- Food/views.py
+++ Food/views.py
@@ -1,14 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import FoodItem
+from django.template import loader
 # Create your views here.
 
 
 def index(request):
     food_item = FoodItem.objects.all()
+    template = loader.get_template('Food/index.html')
+    context = {
+        'food_item': food_item
+    }
+    return HttpResponse(template.render(context, request))
 
-    return HttpResponse(food_item)
 
+def foodId(request, food_id):
 
-# def hey(request):
-#     return HttpResponse("Hello 2, welcome to the Food App!")
+    food_description = FoodItem.objects.get(id=food_id)
+    resp = str(food_id) + " " + food_description.itemDescription
+    return HttpResponse(resp)
